3df0c843e7c97fa343914b67041ce140075ea190
Ferbo7 <ferchubosch@gmail.com>
2023-10-30 17:34:55 +0800

    se creo una variable que pide un numero y se mostro en pantalla si es divisible por 2, 3, 5 o 7

---
+++ js/ejercicioTP10.js
@@ -0,0 +1,12 @@
+let numero = parseInt(prompt("ingrese un numero"));
+
+
+if(numero % 2 === 0){
+    alert(numero + " es divisible por 2");
+} else if(numero % 3 === 0){
+    alert(numero + " es divisible por 3");
+} else if(numero % 5 === 0){
+    alert(numero + " es divisible por 5");
+} else if(numero % 7 === 0){
+    alert(numero + " es divisible por 7");
+}
